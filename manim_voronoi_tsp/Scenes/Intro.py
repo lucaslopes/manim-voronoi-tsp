53096ce4df2eda45d0c75b93a8ea5e4524794617
Lucas Lopes Felipe <lucaslopesf2@gmail.com>
2023-10-07 22:59:02 -0300

    Intro scene with better text description

--- manim_voronoi_tsp/Scenes/Intro.py
+++ manim_voronoi_tsp/Scenes/Intro.py
@@ -1,7 +1,18 @@
-from manim import Scene, Create, Circle, Text
+from manim import Scene, Create, Uncreate, Circle, Text
 
 class Intro(Scene):
-    def construct(self):
+    def construct(self, n_cities=10):
         title = Text("Voronoi TSP").scale(2)
-        self.add(title)
-        self.wait(2)
+        subtitle = Text("A Voronoi-based solution to the Travelling Salesman Problem").scale(0.75)
+        num_cities = Text(f"{n_cities} cities").scale(0.75)
+        self.play(Create(title))
+        self.play(Create(subtitle))
+        self.wait(1)
+        self.play(
+            Uncreate(title),
+            Uncreate(subtitle),
+            Create(num_cities),
+        )
+        self.wait(1)
+        self.play(Uncreate(num_cities))
+        self.wait(1)
