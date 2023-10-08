26dc9fbf4c92d69fe857d6f8bee37421fdb4e8a2
Lucas Lopes Felipe <lucaslopesf2@gmail.com>
2023-10-08 00:42:19 -0300

    First version of the main scence (all sub-scenes)

--- manim_voronoi_tsp/Scenes/Intro.py
+++ manim_voronoi_tsp/Scenes/Intro.py
@@ -1,18 +1,19 @@
-from manim import Scene, Create, Uncreate, Circle, Text
+from manim import Scene, Create, Uncreate, Circle, Text, DOWN
 
 class Intro(Scene):
-    def construct(self, n_cities=10):
+    def construct(self, n_cities=100):
         title = Text("Voronoi TSP").scale(2)
-        subtitle = Text("A Voronoi-based solution to the Travelling Salesman Problem").scale(0.75)
-        num_cities = Text(f"{n_cities} cities").scale(0.75)
+        subtitle = Text(
+            "A Voronoi-based solution to the Travelling Salesman Problem"
+        ).scale(0.75). next_to(title, DOWN)
+        num_cities = Text(f"{n_cities} cities").scale(1)
         self.play(Create(title))
         self.play(Create(subtitle))
         self.wait(1)
         self.play(
-            Uncreate(title),
             Uncreate(subtitle),
-            Create(num_cities),
+            title.animate.become(num_cities).scale(1.5),
         )
         self.wait(1)
-        self.play(Uncreate(num_cities))
-        self.wait(1)
+        self.play(Uncreate(title))
+        self.wait(.1)
